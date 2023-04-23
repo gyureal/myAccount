3e7be2d50ff137f409eecc03636bca9bfac813ed
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-23 22:37:32 +0900

    계좌 조회 컨트롤러 테스트

--- src/test/java/com/example/myaccount/controller/AccountControllerTest.java
+++ src/test/java/com/example/myaccount/controller/AccountControllerTest.java
@@ -18,6 +18,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import redis.embedded.RedisServer;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -58,10 +60,10 @@ class AccountControllerTest {
         // when
         // then
         mockMvc.perform(post("/account")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(
-                        new CreateAccount.Request(12345L, 100L))
-                ))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new CreateAccount.Request(12345L, 100L))
+                        ))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.userId").value(12345L))
                 .andExpect(jsonPath("$.accountNumber").value("1234567"))
@@ -90,6 +92,41 @@ class AccountControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.userId").value(12345L))
                 .andExpect(jsonPath("$.accountNumber").value("1234567890"))
+                .andDo(print());
+    }
+
+    @Test
+    void getAccountsByUserId_계좌_조회_성공() throws Exception {
+
+        List<AccountDto> accountDtos =
+                Arrays.asList(AccountDto.builder()
+                                .accountNumber("1111111111")
+                                .balance(1000L)
+                                .build(),
+                        AccountDto.builder()
+                                .accountNumber("2222222222")
+                                .balance(2222L)
+                                .build(),
+                        AccountDto.builder()
+                                .accountNumber("3333333333")
+                                .balance(3333L)
+                                .build()
+                );
+
+        // given
+        given(accountService.getAccountsByUserId(anyLong()))
+                .willReturn(accountDtos);
+
+        // when
+        // then
+        mockMvc.perform(get("/account?user_id=1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].accountNumber").value("1111111111"))
+                .andExpect(jsonPath("$[0].balance").value(1000L))
+                .andExpect(jsonPath("$[1].accountNumber").value("2222222222"))
+                .andExpect(jsonPath("$[1].balance").value(2222L))
+                .andExpect(jsonPath("$[2].accountNumber").value("3333333333"))
+                .andExpect(jsonPath("$[2].balance").value(3333L))
                 .andDo(print());
     }
 
