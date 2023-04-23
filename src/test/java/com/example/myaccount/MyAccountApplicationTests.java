909783939bbc192ed7a0536ecbbb84fc67eae472
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-23 14:05:44 +0900

    Initial commit

---
+++ src/test/java/com/example/myaccount/MyAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
