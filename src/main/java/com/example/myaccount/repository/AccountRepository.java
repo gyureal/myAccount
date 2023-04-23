a8536d41fca01507db93e518b5e1341f8f59da5b
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-23 17:58:35 +0900

    계좌 가장 처음 것 가져오는 메서드 추가

--- src/main/java/com/example/myaccount/repository/AccountRepository.java
+++ src/main/java/com/example/myaccount/repository/AccountRepository.java
@@ -4,6 +4,9 @@ import com.example.myaccount.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findFirstByOrderByIdDesc(); // 명에 맞게 쿼리 만들어짐
 }
