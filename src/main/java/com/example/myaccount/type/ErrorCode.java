7a3d8383c01345f22ae8c96e79a4067833d1de67
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-23 20:27:51 +0900

    createAccount_계좌_수가_10개_이상일때_예외를_던진다

--- src/main/java/com/example/myaccount/type/ErrorCode.java
+++ src/main/java/com/example/myaccount/type/ErrorCode.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    USER_NOT_FOUND("사용자가 없습니다.");
+    USER_NOT_FOUND("사용자가 없습니다."),
+    MAX_ACCOUNT_PER_USER("한 유저의 최대 계좌 수는 10개 입니다.");
 
     private final String description;
 }
--- src/test/java/com/example/myaccount/service/AccountServiceTest.java
+++ src/test/java/com/example/myaccount/service/AccountServiceTest.java
@@ -4,8 +4,10 @@ import com.example.myaccount.domain.Account;
 import com.example.myaccount.domain.AccountStatus;
 import com.example.myaccount.domain.AccountUser;
 import com.example.myaccount.dto.AccountDto;
+import com.example.myaccount.exception.AccountException;
 import com.example.myaccount.repository.AccountRepository;
 import com.example.myaccount.repository.AccountUserRepository;
+import com.example.myaccount.type.ErrorCode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -36,7 +38,7 @@ class AccountServiceTest {
     private AccountService accountService;  // 위에 모킹한 라이브러리 삽입
 
     @Test
-    void 계좌_생성에_성공한다() {
+    void createAccount_계좌_생성에_성공한다() {
         // given
         Optional<AccountUser> userData = Optional.of(AccountUser.builder()
                 .id(12345L).build());
@@ -66,7 +68,7 @@ class AccountServiceTest {
     }
 
     @Test
-    void 계좌가_없을때_초기_계좌번호로_생성한다() {
+    void createAccount_계좌가_없을때_초기_계좌번호로_생성한다() {
         String INITIAL_ACCOUNT_NUMBER = "1000000000";
 
         // given
@@ -94,6 +96,46 @@ class AccountServiceTest {
         verify(accountRepository, times(1)).save(captor.capture());
         assertEquals(12345L, accountDto.getUserId());
         assertEquals(INITIAL_ACCOUNT_NUMBER, captor.getValue().getAccountNumber());
+    }
+
+    @Test
+    void createAccount_유저_정보가_없을때_예외를_던진다() {
+        String INITIAL_ACCOUNT_NUMBER = "1000000000";
+
+        // given
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.empty());
+
+
+        // when
+        AccountException exception = assertThrows(AccountException.class,
+                () -> accountService.createAccount(1L, 100L));
+
+        // then
+        assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    void createAccount_계좌_수가_10개_이상일때_예외를_던진다() {
+        String INITIAL_ACCOUNT_NUMBER = "1000000000";
+
+        // given
+        Optional<AccountUser> userData = Optional.of(AccountUser.builder()
+                .id(12345L).build());
+
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(userData);
+
+        given(accountRepository.countByAccountUser(any()))
+                .willReturn(11);
+
+
+        // when
+        AccountException exception = assertThrows(AccountException.class,
+                () -> accountService.createAccount(1L, 100L));
+
+        // then
+        assertEquals(ErrorCode.MAX_ACCOUNT_PER_USER, exception.getErrorCode());
     }
 
 }
