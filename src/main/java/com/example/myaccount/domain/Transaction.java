94cda34520b47b49fb1d47fa7e1df5dde5864776
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-24 20:14:57 +0900

    거래 엔티티 생성

---
+++ src/main/java/com/example/myaccount/domain/Transaction.java
@@ -0,0 +1,43 @@
+package com.example.myaccount.domain;
+
+import com.example.myaccount.type.TransactionResultType;
+import com.example.myaccount.type.TransactionType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Transaction {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionResultType transactionResultType;
+    @ManyToOne
+    private Account account;
+    private Long amount;
+    private Long balanceSnapShot;
+    private String transactionId;
+    private LocalDateTime transactionAt;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
