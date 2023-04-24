7c0dd580cbd5cd67c0dcf0b35809758675c18c61
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-24 20:18:28 +0900

    거래 리포지토리 생성

---
+++ src/main/java/com/example/myaccount/repository/TransactionRepository.java
@@ -0,0 +1,15 @@
+package com.example.myaccount.repository;
+
+import com.example.myaccount.domain.Account;
+import com.example.myaccount.domain.AccountUser;
+import com.example.myaccount.domain.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+}
