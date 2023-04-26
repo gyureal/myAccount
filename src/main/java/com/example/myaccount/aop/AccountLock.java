495983b3816828eb591a57edf3745eadf6c211d2
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-26 20:36:16 +0900

    aop aspect 서비스 추가

---
+++ src/main/java/com/example/myaccount/aop/AccountLock.java
@@ -0,0 +1,11 @@
+package com.example.myaccount.aop;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface AccountLock {
+    long tryLockTime() default 5000L; // 락 시간
+}
