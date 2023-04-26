3e270c7f2ba9a5a02846cfb8544a20067e71443b
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-26 20:55:13 +0900

    LockAopAspect 구현 - accountNumber를 argument로 받아오기 - lockService의 파라메티로 넣어주기 - pjp.proceed()에서는 해당 어노테이션이 적용된 실제 로직 수행됨

--- src/main/java/com/example/myaccount/service/LockAopAspect.java
+++ src/main/java/com/example/myaccount/service/LockAopAspect.java
@@ -1,5 +1,6 @@
 package com.example.myaccount.service;
 
+import com.example.myaccount.aop.AccountLockInterface;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -12,17 +13,20 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @RequiredArgsConstructor
 public class LockAopAspect {
+    private  final LockService lockService;
 
-    @Around("@annotation(com.example.myaccount.aop.AccountLock)")
+    @Around("@annotation(com.example.myaccount.aop.AccountLock) && args(request)")
     public Object aroundMethod(
-            ProceedingJoinPoint pjp
+            ProceedingJoinPoint pjp,
+            AccountLockInterface request
     ) throws Throwable {
         // lock 취득 시도
-
+        lockService.lock(request.getAccountNumber());
         try {
             return pjp.proceed();
         } finally {
             // lock 해제
+            lockService.unLock(request.getAccountNumber());
         }
     }
 }
